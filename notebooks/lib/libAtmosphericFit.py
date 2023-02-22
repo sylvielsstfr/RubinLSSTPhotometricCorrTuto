c10a9782466514c34f6e4a3f733e8b470472f498
dagoret <dagoret@lal.in2p3.fr>
2023-02-22 14:32:53 +0100

    update libAtmosphericFit.py to fit atmospheric transmission

--- notebooks/lib/libAtmosphericFit.py
+++ notebooks/lib/libAtmosphericFit.py
@@ -3,6 +3,7 @@
 # Author          : Sylvie Dagoret-Campagne
 # Affiliaton      : IJCLab/IN2P3/CNRS
 # Creation Date   : 2023/02/18
+# Last update     : 2023/02/22
 #
 # A python tool to fit quickly atmospheric transmission of Auxtel Spectra with scipy.optimize and
 # using the atmospheric emulator atmosphtransmemullsst
@@ -13,371 +14,28 @@ import os
 import sys
 from pathlib import Path
 
-#import atmosphtransmemullsst
-#from atmosphtransmemullsst.simpleatmospherictransparencyemulator import SimpleAtmEmulator
+import atmosphtransmemullsst
+from atmosphtransmemullsst.simpleatmospherictransparencyemulator import SimpleAtmEmulator
 
 from scipy.optimize import curve_fit,least_squares
 from scipy.interpolate import RegularGridInterpolator
 import numpy as np
 import pickle
 
-dir_path_data = "/../data/simplegrid" 
 
 
-file_data_dict = {
-    "info_training" :"atmospherictransparencygrid_params_training.pickle",
-    "info_test" : "atmospherictransparencygrid_params_test.pickle",
-    "data_rayleigh_training" : "atmospherictransparencygrid_rayleigh_training.npy",
-    "data_rayleigh_test" : "atmospherictransparencygrid_rayleigh_test.npy",
-    "data_o2abs_training" : "atmospherictransparencygrid_O2abs_training.npy",
-    "data_o2abs_test" : "atmospherictransparencygrid_O2abs_test.npy",
-    "data_pwvabs_training": "atmospherictransparencygrid_PWVabs_training.npy",
-    "data_pwvabs_test":"atmospherictransparencygrid_PWVabs_test.npy",
-    "data_ozabs_training" : "atmospherictransparencygrid_OZabs_training.npy",
-    "data_ozabs_test" : "atmospherictransparencygrid_OZabs_test.npy"
-}
 
-def find_data_path():
-    """
-    Search the path for the atmospheric emulator
-    """
+print("libAtmosphericFit.py :: Use atmosphtransmemullsst.__path__[0],'../data/simplegrid as the path to data")
+data_path = os.path.join(atmosphtransmemullsst.__path__[0],'../data/simplegrid')
+print(f"libAtmosphericFit.py :: data_path = {data_path}")
+
+       
+# The emulator as a global variable
+emul = SimpleAtmEmulator(data_path)
+
+#######################     Fuctions for fitting   ###########################################
     
-    print("relativ data path ",dir_path_data)
-    
-    dir_file_abspath = os.path.dirname(os.path.abspath(__file__))
-    print("abspath = ",dir_file_abspath)
-
-    dir_file_realpath = os.path.dirname(os.path.realpath(__file__))
-    print("realpath = ",dir_file_realpath)
-
-    dir_file_sys = Path(sys.path[0])
-    print("syspath = ",dir_file_sys)
-
-    dir_file_dirname = os.path.dirname(__file__) 
-    print("dirname = ",dir_file_dirname)
-    
-    path_data = dir_file_realpath + dir_path_data
-    
-    for key, filename in file_data_dict.items():
-        file_path = os.path.join(path_data ,  filename)
-        flag_found = os.path.isfile(file_path)  
-        if flag_found :
-            print(f"found data file {file_path}")
-        else:
-            print(f">>>>>>>>>> NOT found data file {file_path}")
-            
-    return path_data 
-
-
-class SimpleAtmEmulator:
-    """
-    Emulate Atmospheric Transparency above LSST from a data grids
-    extracted from libradtran and analytical functions for aerosols.
-    There are 3 grids:
-    - 2D grid Rayleigh transmission vs (wavelength,airmass)
-    - 2D grid O2 absorption vs  (wavelength,airmass)
-    - 3D grid for PWV absorption vs (wavelength,airmass,PWV)
-    - 3D grid for Ozone absorption vs (wavelength,airmass,Ozone)
-    - Aerosol transmission for any number of components
-    
-    """
-    def __init__(self,path='../data/simplegrid'):
-        """
-        Initialize the class for data point files from which the 2D and 3D grids are created.
-        Interpolation are calculated from the scipy RegularGridInterpolator() function
-        """
-        
-        self.path = path
-        self.fn_info_training = file_data_dict["info_training"]
-        self.fn_info_test = file_data_dict["info_test"]
-        self.fn_rayleigh_training = file_data_dict["data_rayleigh_training"]
-        self.fn_rayleigh_test = file_data_dict["data_rayleigh_test"]
-        self.fn_O2abs_training = file_data_dict["data_o2abs_training"]
-        self.fn_O2abs_test = file_data_dict["data_o2abs_test"]
-        self.fn_PWVabs_training = file_data_dict["data_pwvabs_training"]
-        self.fn_PWVabs_test = file_data_dict[ "data_pwvabs_test"]
-        self.fn_OZabs_training = file_data_dict["data_ozabs_training"]
-        self.fn_OZabs_test = file_data_dict["data_ozabs_test"]
-        
-
-        self.info_params_training = None
-        self.info_params_test = None
-        self.data_rayleigh_training = None
-        self.data_rayleigh_test = None
-        self.data_O2abs_training = None
-        self.data_O2abs_test = None
-        self.data_PWVabs_training = None
-        self.data_PWVabs_test = None
-        self.data_OZabs_training = None
-        self.data_OZabs_test = None
-        
-        self.loadtables()
-        
-        self.WLMIN = self.info_params_training["WLMIN"]
-        self.WLMAX = self.info_params_training["WLMAX"]
-        self.WLBIN = self.info_params_training["WLBIN"]
-        self.NWLBIN = self.info_params_training['NWLBIN']
-        self.WL = self.info_params_training['WL']
-        
-        self.AIRMASSMIN = self.info_params_training['AIRMASSMIN']
-        self.AIRMASSMAX = self.info_params_training['AIRMASSMAX']
-        self.NAIRMASS = self.info_params_training['NAIRMASS']
-        self.DAIRMASS = self.info_params_training['DAIRMASS']
-        self.AIRMASS = self.info_params_training['AIRMASS']
-        
-        self.PWVMIN = self.info_params_training['PWVMIN']
-        self.PWVMAX = self.info_params_training['PWVMAX'] 
-        self.NPWV = self.info_params_training['NPWV']
-        self.DPWV = self.info_params_training['DPWV'] 
-        self.PWV = self.info_params_training['PWV']
-        
-        
-        self.OZMIN =  self.info_params_training['OZMIN']
-        self.OZMAX = self.info_params_training['OZMAX']
-        self.NOZ = self.info_params_training['NOZ']
-        self.DOZ =  self.info_params_training['DOZ'] 
-        self.OZ = self.info_params_training['OZ']
-        
-        
-        self.lambda0 = 550.
-        self.tau0 = 1.
-
-
-        self.func_rayleigh_train = RegularGridInterpolator((self.WL,self.AIRMASS),self.data_rayleigh_training)
-        self.func_O2abs_train = RegularGridInterpolator((self.WL,self.AIRMASS),self.data_O2abs_training)
-        self.func_PWVabs_train = RegularGridInterpolator((self.WL,self.AIRMASS,self.PWV),self.data_PWVabs_training)
-        self.func_OZabs_train = RegularGridInterpolator((self.WL,self.AIRMASS,self.OZ),self.data_OZabs_training)
-
-        
-        
-    def loadtables(self):
-        """
-        Load files into grid arrays
-        """
-        
-        filename=os.path.join(self.path,self.fn_info_training)     
-        with open(filename, 'rb') as f:
-            self.info_params_training = pickle.load(f)
-            
-        filename=os.path.join(self.path,self.fn_info_test)     
-        with open(filename, 'rb') as f:
-            self.info_params_test = pickle.load(f)        
-        
-        filename=os.path.join(self.path,self.fn_rayleigh_training)
-        with open(filename, 'rb') as f:
-            self.data_rayleigh_training=np.load(f)
-            print("data_rayleigh_training",self.data_rayleigh_training.shape)
-            
-        filename=os.path.join(self.path,self.fn_rayleigh_test)
-        with open(filename, 'rb') as f:
-            self.data_rayleigh_test=np.load(f)
-            print("data_rayleigh_test",self.data_rayleigh_test.shape)
-            
-        filename=os.path.join(self.path,self.fn_O2abs_training)
-        with open(filename, 'rb') as f:
-            self.data_O2abs_training=np.load(f)
-            
-        filename=os.path.join(self.path,self.fn_O2abs_test)
-        with open(filename, 'rb') as f:
-            self.data_O2abs_test=np.load(f)
-                  
-        filename=os.path.join(self.path,self.fn_PWVabs_training)
-        with open(filename, 'rb') as f:
-            self.data_PWVabs_training=np.load(f)
-            
-        filename=os.path.join(self.path,self.fn_PWVabs_test)
-        with open(filename, 'rb') as f:
-            self.data_PWVabs_test=np.load(f)
-            
-            
-        filename=os.path.join(self.path,self.fn_OZabs_training)
-        with open(filename, 'rb') as f:
-            self.data_OZabs_training=np.load(f)
-            
-        filename=os.path.join(self.path,self.fn_OZabs_test)
-        with open(filename, 'rb') as f:
-            self.data_OZabs_test=np.load(f)
-            
-            
-    def GetWL(self):
-        return self.WL
-            
-    def GetRayleighTransparencyArray(self,wl,am):
-        #pts = np.array([ np.array([the_wl,am],dtype=object)  for the_wl in wl ],dtype=object) 
-        #return np.array([ self.func_rayleigh_train(pt) for pt in pts])
-        pts = [ (the_wl,am) for the_wl in wl ]
-        pts = np.array(pts)
-        return self.func_rayleigh_train(pts)
-    
-    
-    def GetO2absTransparencyArray(self,wl,am):
-        #pts = np.array([ np.array([the_wl,am],dtype=object)   for the_wl in wl],dtype=object)
-        #return np.array([ self.func_O2abs_train(pt) for pt in pts])
-        pts = [ (the_wl,am) for the_wl in wl ]
-        pts = np.array(pts)
-        return self.func_O2abs_train(pts)
-    
-    
-    def GetPWVabsTransparencyArray(self,wl,am,pwv):
-        #pts = np.array([ np.array([the_wl,am,pwv],dtype=object)   for the_wl in wl],dtype=object)
-        #return  np.array([self.func_PWVabs_train(pt) for pt in pts])
-        pts = [ (the_wl,am,pwv) for the_wl in wl ]
-        pts = np.array(pts)
-        return self.func_PWVabs_train(pts)
-    
-    
-    def GetOZabsTransparencyArray(self,wl,am,oz):
-        #pts = np.array([ np.array([the_wl,am,oz],dtype=object)   for the_wl in wl],dtype=object)
-        #return np.array([self.func_OZabs_train(pt) for pt in pts])
-        pts = [ (the_wl,am,oz) for the_wl in wl ]
-        pts = np.array(pts)
-        return self.func_OZabs_train(pts)
-    
-    
-    def GetGriddedTransparencies(self,wl,am,pwv,oz,flagRayleigh=True,flagO2abs=True,flagPWVabs=True,flagOZabs=True):
-        """
-        Emulation of libradtran simulated transparencies. Decomposition of the
-        total transmission in different processes:
-        - Rayleigh scattering
-        - O2 absorption
-        - PWV absorption
-        - Ozone absorption
-        
-        inputs:
-        - wl : wavelength array or list
-        - am :the airmass,
-        - pwv : the precipitable water vapor (mm)
-        - oz : the ozone column depth in Dobson unit
-        - flags to activate or not the individual interaction processes
-        
-        outputs:
-        - 1D array of atmospheric transmission (save size as wl)
-        
-        """
-        
-
-        if flagRayleigh:
-            transm = self.GetRayleighTransparencyArray(wl,am)
-        else:
-            transm = np.ones(len(wl))
-            
-        if flagO2abs:
-            transm *= self.GetO2absTransparencyArray(wl,am)
-            
-        if flagPWVabs:
-            transm *= self.GetPWVabsTransparencyArray(wl,am,pwv)
-            
-        if flagOZabs:
-            transm *= self.GetOZabsTransparencyArray(wl,am,oz)
-            
-        # if pts are array of array, RegularGridInterpolator returns an array of (n,1)
-        # In that case, one must flatten the array
-        if transm.ndim >= 2:
-            transm =transm.squeeze()
-        return transm
-            
-    def GetAerosolsTransparencies(self,wl,am,ncomp,taus=None,betas=None):
-        """
-        Compute transmission due to aerosols:
-        
-        inputs:
-        - wl : wavelength array
-        - am : the airmass
-        - ncomp : the number of aerosol components
-        - taus : the vertical aerosol depth of each component at lambda0 vavelength
-        - betas : the angstrom exponent. Must be negativ.
-        
-        
-        outputs:
-        - 1D array of atmospheric transmission (save size as wl)
-        
-        """
-        if not isinstance(wl, np.ndarray):   
-            wl = np.array(wl,dtype=object)
-        NWL=wl.shape[0]
-        
-        transm = np.ones(NWL)
-        
-        if ncomp <=0:
-            return transm
-        else:
-            taus=np.array(taus)
-            betas=np.array(betas)
-            
-            NTAUS=taus.shape[0]
-            NBETAS=betas.shape[0]
-        
-            assert ncomp<=NTAUS
-            assert ncomp<=NBETAS     
-        
-            for icomp in range(ncomp):            
-                exponent = (taus[icomp]/self.tau0)*np.exp(betas[icomp]*np.log(wl/self.lambda0))*am
-                transm *= np.exp(-exponent)
-            
-            return transm
-        
-        
-    def GetAllTransparencies(self,wl,am,pwv,oz,ncomp=0, taus=None, betas=None, flagRayleigh=True,flagO2abs=True,flagPWVabs=True,flagOZabs=True,flagAerosols=False):
-        """
-        Combine interpolated libradtran transmission with analytical expression for the
-        aerosols
-        
-        inputs:
-        - wl : wavelength array or list
-        - am :the airmass,
-        - pwv : the precipitable water vapor (mm)
-        - oz : the ozone column depth in Dobson unit
-        - ncomp : number of aerosols components,
-        - taus & betas : arrays of parameters for aerosols
-        - flags to activate or not the individual interaction processes
-        
-        outputs:
-        - 1D array of atmospheric transmission (save size as wl)
-        
-        """
-        
-        
-        transm = self.GetGriddedTransparencies(wl,am,pwv,oz,flagRayleigh=flagRayleigh,flagO2abs=flagO2abs,flagPWVabs=flagPWVabs,flagOZabs=flagOZabs)
-        
-        if flagAerosols:
-            transmaer = self.GetAerosolsTransparencies(wl,am,ncomp,taus,betas)
-            transm *=transmaer
-           
-            
-        return transm
-            
-print("Path at terminal when executing this file")
-print(os.getcwd() + "\n")
-current_path = os.getcwd()
-
-print("This file path, relative to os.getcwd()")
-print(__file__ + "\n")
-
-print("This file full path (following symlinks)")
-full_path = os.path.realpath(__file__)
-print(full_path + "\n")
-
-print("This file directory and name")
-path, filename = os.path.split(full_path)
-print(path + ' --> ' + filename + "\n")
-
-print("This file directory only")
-print(os.path.dirname(full_path))
-
-print("The data path is")
-data_path = current_path + "/../data/simplegrid"   
-print(data_path)
-        
-# global variable
-#emul = SimpleAtmEmulator(os.path.join(atmosphtransmemullsst.__path__[0],'../data/simplegrid'))
-#emul = SimpleAtmEmulator(path='/Users/sylvie/MacOSX/GitHub/LSST/AuxTelComm/notebooks_usdf/FitAtmosphericParameters/data/simplegrid')
-emul = SimpleAtmEmulator(path=data_path)
-
-#emul = SimpleAtmEmulator(path='/Users/dagoret/MacOSX/GitHub/LSST/AuxTelComm/notebooks_usdf/FitAtmosphericParameters/data/simplegrid')
- 
- 
-    
-# =========    Functions for the fit grey factor, pwv, ozone ========================
+# =================    Functions for the fit grey factor, pwv, ozone ========================
 def fluxpred_greypwvo3(params,*arg):
     """
     Model prediction y = f(x) where x is the array of wavelength and y the measured flux
@@ -460,7 +118,7 @@ def func_residuals_greypwvo3(params,*arg):
     
     return residuals
 
-# =========    Functions for the fit grey factor, pwv, No ozone ========================
+# ================    Functions for the fit grey factor, pwv, No ozone ========================
 def fluxpred_greypwv(params,*arg):
     """
     Model prediction y = f(x) where x is the array of wavelength and y the measured flux
@@ -542,9 +200,70 @@ def func_residuals_greypwv(params,*arg):
     
     return residuals
 
+# ================    Functions for the atmospheric profile airmass, pwv, ozone, aerosols ========
+def transmpred_ampwvozaer(params,*arg):
+    """
+    Model prediction y = f(x) where x is the array of wavelength and y the measured flux
+    
+    inputs:
+      - params : the unknown atmospheric parameters to fit (am,pwv,oz,vaod,beta)
+      - *args  : additionnal data provided to compute the model : 
+                   1) the wavelength array, 
+                  
+      
+    output:
+      - the array of predicted air transmission  at each wavelength data point
+      
+    """
+    
+    global emul
+    
+    # decode parameters
+    am,pwv,oz,vaod,beta = params   
+    
+    # decode the argument
+    wl = arg[0]
+    
+    transm = emul.GetAllTransparencies(wl ,am,pwv,oz,ncomp=1,taus=[vaod],betas=[beta],flagAerosols=True)
+     
+    return transm
 
 
-# 
+def func_residuals_tramampwvozaer(params,*arg):
+    """
+    This function is called by the scipy.optimize.least_squares function to compute the normalized residuals
+    at each data point at each wavelength (yi-f(xi,thetaj)/sigmai
+    
+    input:
+      - params : the unknown atmospheric parameters to fit (pwv,oz)
+      - *args  : additionnal data provided to compute the mode : 
+                 to the model
+                   1) the wavelength array, 
+
+                  
+    
+    return:
+      - the array of residuals for the given set of parameters required by the least_squares function 
+    """
+    
+    
+    
+    the_wl = arg[0]         # wavelength array required by the model function pred2
+    the_data = arg[1]       # the observed fluxes array
+    
+    
+     
+    am,pwv,oz,vaod,beta = params      # decode the parameters
+    
+    
+    transm_model = transmpred_ampwvozaer(params,the_wl)
+    
+    
+    residuals = (the_data - transm_model)
+    
+    return residuals
+
+########################## Fitting class ############################################ 
 class FitAtmosphericParams:
     """
     Class to handle a buch of different methods for fitting
@@ -589,7 +308,7 @@ class FitAtmosphericParams:
         flux_model = fluxpred_greypwvo3(params,xdata,airmass,sedxthroughput)
         return flux_model
     
-#--------
+    #--------
     
     def fit_greypwv(self,params0,xdata,ydata,yerrdata,airmass,sedxthroughput):
         """
@@ -625,8 +344,41 @@ class FitAtmosphericParams:
         """
         flux_model = fluxpred_greypwv(params,xdata,airmass,sedxthroughput)
         return flux_model
+    
+    
+    #-------------------
+
+    def fit_transmampwvozaer(self,params0,xdata,ydata):
+        """
+        Fit transmission
+
+        """            
+        res_fit = least_squares(func_residuals_tramampwvozaer, params0,bounds=([1.001,0.001,0.001,0,-3],[2.499,9.5,599,0.5,0]),args=[xdata,ydata])
+        
+        # results fo the fit
+        popt= res_fit.x
+        J = res_fit.jac
+        pcov = np.linalg.inv(J.T.dot(J))
+        sigmas = np.sqrt(np.diagonal(pcov))
+        cost=res_fit.cost
+        residuals = res_fit.fun
+        chi2=np.sum(residuals**2)
+        ndeg = J.shape[0]- popt.shape[0]
+        chi2_per_deg = chi2/ndeg
         
         
+        fit_dict = {"chi2":chi2,"ndeg":ndeg,"chi2_per_deg":chi2_per_deg,"popt":popt,"sigmas":sigmas}
+
+        return res_fit,fit_dict
+    
+    
+    def pred_transmampwvozaer(self,params,xdata):
+        """
+        """
+        transm_model = transmpred_ampwvozaer(params,xdata)
+        return transm_model
+        
+################################################################################################        
 
 
 def main():
@@ -635,7 +387,7 @@ def main():
     print("============================================================")
     
     # retrieve the path of data
-    path_data =  find_data_path()  
+    path_data =  os.path.join(atmosphtransmemullsst.__path__[0],'../data/simplegrid')
     # create emulator  
     emul = SimpleAtmEmulator(path = path_data)
     wl = [400.,800.,900.]
