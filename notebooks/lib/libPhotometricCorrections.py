eb8cffc36f6a91bfb4218e79b81f6ab44dd6fda0
dagoret <dagoret@lal.in2p3.fr>
2023-02-24 18:34:06 +0100

    expand libPhotometricCorrections.py

--- notebooks/lib/libPhotometricCorrections.py
+++ notebooks/lib/libPhotometricCorrections.py
@@ -74,18 +74,26 @@ emul_atm = SimpleAtmEmulator(data_path)
 
 
 class PhotometricCorrections:
-  def __init__(self,am0=1.2,pwv0=5.0,oz0=300,ncomp=1,tau0=0.04,beta0=-1):
+  def __init__(self,am0=1.2,pwv0=5.0,oz0=300,ncomp0=1,tau0=0.04,beta0=-1):
         """
         Constructor
         """
         global emul_atm,sed
         self.WL = emul_atm.GetWL()
         
+        # standard atmosphere parameters
+        self.am0 = am0
+        self.pwv0 = pwv0
+        self.oz0 = oz0
+        self.ncomp0 = ncomp0
+        self.tau0 = tau0
+        self.beta0 = beta0
+        
         # standard atmosphere
-        if ncomp == 1:
+        if ncomp0 == 1:
           taus = [tau0]
           betas = [beta0]    
-          self.atm_std = emul_atm.GetAllTransparencies(self.WL,am0,pwv0,oz0,ncomp=ncomp, taus=taus, betas=betas, flagAerosols=True)
+          self.atm_std = emul_atm.GetAllTransparencies(self.WL,am0,pwv0,oz0,ncomp=ncomp0, taus=taus, betas=betas, flagAerosols=True)
         else:
           self.atm_std = emul_atm.GetAllTransparencies(self.WL,am0,pwv0,oz0)
           
@@ -119,7 +127,14 @@ class PhotometricCorrections:
           self.all_II1_std[f] = the_II1
           
           
-        # Non standard calculations will be caculated later
+        # Non standard calculations will be calculated later after initialisation
+        self.am = 1.2
+        self.pwv = 0
+        self.oz = 0
+        self.ncomp = 1
+        self.tau = 0.04
+        self.beta = -1
+        
         self.atm_nonstd = None
         self.bandpass_total_nonstd = None
         self.phiArray_nonstd = None
@@ -127,6 +142,14 @@ class PhotometricCorrections:
         self.all_II1_nonstd = None
         self.all_II0ratio_nonstd = None
         self.all_II1sub_nonstd = None
+        
+        self.coll_atm_nonstd = None
+        self.coll_bandpass_total_nonstd = None
+        self.coll_phiArray_nonstd = None
+        self.coll_all_II0_nonstd = None
+        self.coll_all_II1_nonstd = None
+        self.coll_all_II0ratio_nonstd = None
+        self.coll_all_II1sub_nonstd = None
         
         
          
@@ -137,10 +160,113 @@ class PhotometricCorrections:
   def fII1(self,wl,phi,wlb):
         return np.trapz(phi*(wl-wlb),wl)
       
-  def CalculateObs(self):
+  def CalculateObs(self,am=1.2,pwv=5.0,oz=300,ncomp=1,tau=0.04,beta=-1):
         """
         """
-        pass
+        self.am = am
+        self.pwv = pwv 
+        self.oz = oz
+        self.ncomp = ncomp
+        self.tau = tau
+        self.beta = beta
+        
+        # non standard atmosphere
+        if ncomp == 1:
+          taus = [tau]
+          betas = [beta]    
+          self.atm_nonstd = emul_atm.GetAllTransparencies(self.WL,am,pwv,oz,ncomp=ncomp, taus=taus, betas=betas, flagAerosols=True)
+        else:
+          self.atm_std = emul_atm.GetAllTransparencies(self.WL,am,pwv,oz)
+          
+        # non standard total filter (instrumental x atmosphere)  
+        self.bandpass_total_nonstd = {} 
+        for index,f in enumerate(filter_tagnames):
+          self.bandpass_total_nonstd[f] = Bandpass(wavelen=self.WL,sb=self.bandpass_inst[f].sb * self.atm_nonstd)
+        
+        # Non standard Normalized response 
+        self.phiArray_nonstd, _ = Sed().setup_phi_array([self.bandpass_total_nonstd[f] for f in filter_tagnames])
+        
+        # Integrals IIb0(non std) and IIb1(non std)
+        self.all_II0_nonstd = {}
+        self.all_II1_nonstd = {}
+        self.all_II0ratio_nonstd = {}
+        self.all_II1sub_nonstd = {}
+        
+        for index,f in enumerate(filter_tagnames):
+    
+          the_II0 = self.fII0(self.bandpass_total_nonstd[f].wavelen,self.bandpass_total_nonstd[f].sb)
+          self.all_II0_nonstd[f] = the_II0
+          self.all_II0ratio_nonstd[f] = the_II0/self.all_II0_std[f]
+          the_II1 = self.fII1(self.WL,self.phiArray_nonstd[index,:],FILTERWL[index,2])
+          self.all_II1_nonstd[f] = the_II1
+          self.all_II1sub_nonstd[f] = self.all_II1_std[f] - the_II1
+          
+  def CalculateMultiObs(self,am,pwv,oz,ncomp,tau,beta):
+        """
+        """
+        
+        self.coll_atm_nonstd = []
+        self.coll_bandpass_total_nonstd = []
+        self.coll_phiArray_nonstd = []
+        self.coll_all_II0_nonstd = []
+        self.coll_all_II1_nonstd = []
+        self.coll_all_II0ratio_nonstd = []
+        self.coll_all_II1sub_nonstd = []
+        
+        if isinstance(am, list) or isinstance(am, np.ndarray):
+          if isinstance(am, list):
+            all_am = np.array(am)
+          else:
+            all_am = am
+          for am in all_am:
+            self.CalculateObs(am,pwv,oz,ncomp,tau,beta)
+            self.coll_atm_nonstd.append(self.atm_nonstd)
+            self.coll_bandpass_total_nonstd.append(self.coll_bandpass_total_nonstd)
+            self.coll_phiArray_nonstd.append(self.phiArray_nonstd)
+            self.coll_all_II0_nonstd.append(self.all_II0_nonstd) 
+            self.coll_all_II1_nonstd.append(self.all_II1_nonstd)
+            self.coll_all_II0ratio_nonstd.append(self.all_II0ratio_nonstd)
+            self.coll_all_II1sub_nonstd.append(self.all_II1sub_nonstd)
+                
+                    
+        elif isinstance(pwv, list) or isinstance(pwv, np.ndarray): 
+          if isinstance(pwv, list):
+            all_pwv = np.array(pwv)
+          else:
+            all_pwv = pwv
+            
+          for pwv in all_pwv:
+            self.CalculateObs(am,pwv,oz,ncomp,tau,beta)
+            self.coll_atm_nonstd.append(self.atm_nonstd)
+            self.coll_bandpass_total_nonstd.append(self.coll_bandpass_total_nonstd)
+            self.coll_phiArray_nonstd.append(self.phiArray_nonstd)
+            self.coll_all_II0_nonstd.append(self.all_II0_nonstd) 
+            self.coll_all_II1_nonstd.append(self.all_II1_nonstd)
+            self.coll_all_II0ratio_nonstd.append(self.all_II0ratio_nonstd)
+            self.coll_all_II1sub_nonstd.append(self.all_II1sub_nonstd)
+                
+         
+        elif isinstance(oz, list) or isinstance(oz, np.ndarray): 
+          if isinstance(oz, list):
+            all_oz = np.array(oz)
+          else:
+            all_oz = oz
+            
+          for oz in all_oz:
+            self.CalculateObs(am,pwv,oz,ncomp,tau,beta)
+            self.coll_atm_nonstd.append(self.atm_nonstd)
+            self.coll_bandpass_total_nonstd.append(self.coll_bandpass_total_nonstd)
+            self.coll_phiArray_nonstd.append(self.phiArray_nonstd)
+            self.coll_all_II0_nonstd.append(self.all_II0_nonstd) 
+            self.coll_all_II1_nonstd.append(self.all_II1_nonstd)
+            self.coll_all_II0ratio_nonstd.append(self.all_II0ratio_nonstd)
+            self.coll_all_II1sub_nonstd.append(self.all_II1sub_nonstd)
+        
+        else:
+          print("Not implemented yet")             
+          
+        
+          
       
            
           
